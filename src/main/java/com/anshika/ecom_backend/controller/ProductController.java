c47f830aab9e1dd2b66347c768274af22aad9886
Anshika Saxena <developer.anshika467@gmail.com>
2025-08-07 15:44:20 +0530

    Implemented GetProductById and date formatting

--- src/main/java/com/anshika/ecom_backend/controller/ProductController.java
+++ src/main/java/com/anshika/ecom_backend/controller/ProductController.java
@@ -3,10 +3,9 @@ package com.anshika.ecom_backend.controller;
 import com.anshika.ecom_backend.model.Product;
 import com.anshika.ecom_backend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,14 +17,20 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-    @RequestMapping("/")
-    public String greet() {
-        return "Hey Billionaire!!!";
+    @GetMapping("/getAllProducts")
+    public ResponseEntity<List<Product>> getAllProducts() {
+
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
 
-    @GetMapping("/getAllProducts")
-    public List<Product> getAllProducts() {
-        return service.getAllProducts();
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
+        Product prod = service.getProductById(id);
+
+        if(prod != null)
+            return new ResponseEntity<>(prod, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 }
